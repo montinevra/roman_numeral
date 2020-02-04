e5e0a4bbe48920269d6fe8c909515bfbfa5d6347
Montinevra <montinevra@gmail.com>
2020-02-04 15:01:33 -0700

    first commit

---
+++ roman_numeral.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+def roman_from_int(t_num):
+	MAX_VALUE = 4000
+	roman_symbols = {1000: "M",
+		900: "CM", 500: "D", 400: "CD", 100: "C",
+		90: "XC", 50: "L", 40: "XL", 10: "X",
+		9: "IX", 5: "V", 4: "IV", 1: "I",
+	}
+	roman = ""
+
+	if t_num >= MAX_VALUE:
+		raise ValueError("input number must be less than " + str(MAX_VALUE))
+		return
+	for i in roman_symbols:
+		while t_num >= i:
+			roman += roman_symbols[i]
+			t_num -= i
+	return roman
+
+
+def main(t_argv):
+	if len(t_argv) > 1:
+		print(roman_from_int(int(sys.argv[1])))
+	else:
+		print("Usage: roman_numeral.py int")
+
+
+if __name__ == "__main__":
+	import sys
+
+	main(sys.argv)
